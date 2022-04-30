c9ac23210cf713158f1c67c65cfac51a16f4e185
Kamiyo <kamiyo@outlook.it>
2022-04-30 12:35:55 +0200

    Added Counter Passive

---
+++ Passives/PassiveAbility_FlowerProtection_21341.cs
@@ -0,0 +1,13 @@
+﻿using KamiyoStaticUtil.Utils;
+using Mary_Ib21341.BLL;
+
+namespace Mary_Ib21341.Passives
+{
+    public class PassiveAbility_FlowerProtection_21341 : PassiveAbilityBase
+    {
+        public override void OnStartBattle()
+        {
+            UnitUtil.ReadyCounterCard(owner, 6, MaryModParameters.PackageId);
+        }
+    }
+}
