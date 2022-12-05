1852a6267ec534301315a8ac10bdc5f643536abb
Kamiyo <kamiyo.k91@gmail.com>
2022-12-05 19:21:42 +0100

    Hotfix

--- Passives/PassiveAbility_GuertenaArt_21341.cs
+++ Passives/PassiveAbility_GuertenaArt_21341.cs
@@ -11,7 +11,7 @@ namespace Mary_Ib21341.Passives
             _count = 1;
             owner.bufListDetail.AddKeywordBufThisRoundByEtc(KeywordBuf.Strength, _count);
             owner.bufListDetail.AddKeywordBufThisRoundByEtc(KeywordBuf.Endurance, _count);
-            if (UnitUtil.SupportCharCheck(owner, true) == 1)
+            if (UnitUtil.SupportCharCheck(owner, true) < 2)
             {
                 owner.bufListDetail.AddKeywordBufThisRoundByEtc(KeywordBuf.Strength, 1);
                 owner.bufListDetail.AddKeywordBufThisRoundByEtc(KeywordBuf.Endurance, 1);
@@ -26,13 +26,13 @@ namespace Mary_Ib21341.Passives
         {
             owner.bufListDetail.AddKeywordBufByEtc(KeywordBuf.Strength, _count);
             owner.bufListDetail.AddKeywordBufByEtc(KeywordBuf.Endurance, _count);
-            if (UnitUtil.SupportCharCheck(owner, true) == 1)
+            if (UnitUtil.SupportCharCheck(owner, true) < 2)
             {
                 owner.bufListDetail.AddKeywordBufByEtc(KeywordBuf.Strength, 1);
                 owner.bufListDetail.AddKeywordBufByEtc(KeywordBuf.Endurance, 1);
             }
 
-            if (UnitUtil.SupportCharCheck(owner) != 1) return;
+            if (UnitUtil.SupportCharCheck(owner) > 1) return;
             owner.bufListDetail.AddKeywordBufByEtc(KeywordBuf.Strength, 1);
             owner.bufListDetail.AddKeywordBufByEtc(KeywordBuf.Endurance, 1);
         }
